3060c448b40618ce3c2a7364fc27775eb5934f3d
Asparuh Kamenov <asparuh@hack.bg>
2021-04-12 16:45:54 +0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+use cosmwasm_std::{CanonicalAddr, HumanAddr, Api, StdResult};
+
+pub mod viewing_key;
+pub mod storage;
+pub mod rand;
+
+mod data;
+
+pub use data::{ContractInfo, ContractInfoStored, Callback, CallbackStored};
+
+/// Attempting to canonicalize an empty address will fail. 
+/// This function skips calling `canonical_address` if the input is empty.
+pub fn canonicalize_maybe_empty(api: &impl Api, addr: &HumanAddr) -> StdResult<CanonicalAddr> {
+    Ok(
+        if *addr == HumanAddr::default() {
+            CanonicalAddr::default()
+        } else {
+            api.canonical_address(addr)?
+        }
+    )
+}
+
+/// Attempting to humanize an empty address will fail. 
+/// This function skips calling `human_address` if the input is empty.
+pub fn humanize_maybe_empty(api: &impl Api, addr: &CanonicalAddr) -> StdResult<HumanAddr> {
+    Ok(
+        if *addr == CanonicalAddr::default() {
+            HumanAddr::default()
+        } else {
+            api.human_address(addr)?
+        }
+    )
+}
